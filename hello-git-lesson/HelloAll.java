77544b529fb39174deff17d6516d9336ff9b2c93
Alexandr Bagriy <sasha.bag@gmail.com>
2019-02-28 22:05:17 +0200

    HelloAll.java

--- hello-git-lesson/HelloAll.java
+++ hello-git-lesson/HelloAll.java
@@ -3,6 +3,7 @@ public class HelloAll {
     	System.out.println("Hello all!!!");
 	//================================
 	printUserInfo("Ruslan", "Gramatic");
+	printUserInfo("Maxim", "Voroniuk");
     }
 
     private static void printUserInfo(String firstName, String lastName) {
