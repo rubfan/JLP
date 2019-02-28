d94d68198eb4b7f04997719a79c27c3f90576236
ibrus <brus.andreevich@gmail.com>
2019-02-28 22:11:49 +0200

    final pizza delivery

--- hello-git-lesson/HelloAll.java
+++ hello-git-lesson/HelloAll.java
@@ -2,6 +2,7 @@ public class HelloAll {
     public static void main(String[] args) {
     	System.out.println("Hello all!!!");
 	//================================
+    printUserInfo("Ivan", "Brus");
 	printUserInfo("Ivan", "Brus");
     }
 
