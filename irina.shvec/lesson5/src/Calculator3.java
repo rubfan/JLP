61d7736616793849dda6526392451a59a3f8b1f7
Irinashvec999 <irinashvec999@gmail.com>
2019-03-11 12:29:03 +0200

    Changed method of solving in Calculator3

--- irina.shvec/lesson5/src/Calculator3.java
+++ irina.shvec/lesson5/src/Calculator3.java
@@ -1,37 +1,42 @@
-import java.io.Console;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Calculator3 {
-   static Console console = System.console();
 
-    public static void main(String[] args) {
-        if(console!=null) {
-            char[] operation = console.readPassword("Enter the operation:");
-            String num1 = console.readLine("Enter the number:");
-            String num2 = console.readLine("Enter the number:");
-            int result = calc(num1, num2, operation);
-            System.out.println("Result of operation: " + result);
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(("Enter the operation:"));
+        char[] operation = br.readLine().toCharArray();
+
+        System.out.println("Enter the number:");
+        int num1 = Integer.parseInt(br.readLine());
+        System.out.println("Enter the number:");
+        int num2 = Integer.parseInt(br.readLine());
+        int result = calc(num1, num2, operation);
+        System.out.println("Result of operation: " + result);
     }
-    public static int calc(String firstOperand, String secondOperand, char [] operation){
+
+    public static int calc(int firstOperand, int secondOperand, char[] operation) {
         int result = 0;
-        switch (operation[0]){
+        switch (operation[0]) {
             case '+':
-                result = Integer.parseInt(firstOperand)+Integer.parseInt(secondOperand);
+                result = firstOperand + secondOperand;
                 break;
             case '-':
-                result = Integer.parseInt(firstOperand)-Integer.parseInt(secondOperand);
+                result = firstOperand - secondOperand;
                 break;
             case '*':
-                result = Integer.parseInt(firstOperand)*Integer.parseInt(secondOperand);
+                result = firstOperand * secondOperand;
                 break;
             case '/':
-                result = Integer.parseInt(firstOperand)/Integer.parseInt(secondOperand);
+                result = firstOperand / secondOperand;
                 break;
             case '%':
-                result = Integer.parseInt(firstOperand)%Integer.parseInt(secondOperand);
+                result = firstOperand % secondOperand;
                 break;
             case '|':
-                result = Math.abs(Integer.parseInt(firstOperand));
+                result = Math.abs(firstOperand);
                 break;
             default:
                 System.out.println("Operation isn't recognized. Please, re-enter");
