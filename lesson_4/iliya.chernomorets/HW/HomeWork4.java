07e67e7fdf9237df543e914aaa4ff31938aed0e9
Илья  Черноморец <ilacernomorec@MacBook-Pro-Ila.local>
2019-03-11 14:57:37 +0200

    added HomeWork4

---
+++ lesson_4/iliya.chernomorets/HW/HomeWork4.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+public class Main {
+
+
+
+
+        public static void main(String[] args) {
+        //System.out.println("Hello World!");
+       /* Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("Минимальное число:" + Math.min(a, Math.min(b,c)));
+        System.out.println("Максимальное число:" + Math.max(a, Math.max(b,c)));*/
+
+
+
+
+
+
+            /*Scanner sc = new Scanner(System.in);
+            System.out.println("Введите число:");
+            int a = sc.nextInt();
+            if (a%2==0) {
+                System.out.println("Четное");
+            }else {
+                System.out.println("Нечетное");
+            }*/
+
+
+            /*Scanner sc = new Scanner(System.in);
+            System.out.println("Введите число:");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a%b==0) {
+                System.out.println("Делится нацело");
+            }else {
+                System.out.println("Не делится нацело");
+            }*/
+
+
+            /*Scanner sc = new Scanner(System.in);
+            System.out.println("Введите число:");
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            double b=Math.abs(10-n);
+            double c=Math.abs(10-m);
+            if (b==c){
+                System.out.println("Числа "+n +" и "+m + " равноудалены от 10 ");
+            }
+            if(b>c){
+                System.out.println("Число "+m+" ближе к 10");
+            }
+            if (b<c) {
+                System.out.println("Число "+n+" ближе к 10");
+            }*/
+
+
+
+            /*Scanner sc = new Scanner(System.in);
+            System.out.println("Введите количество дней:");
+            int a = sc.nextInt();
+            int b= a*40-50;
+            int c = a*40-20;
+            if(a > 7){
+                System.out.println("Сумма ренты " + b);
+            }
+            else if(a > 5 && a < 7){
+                System.out.println("Сумма ренты " + c);
+            }
+            else{System.out.println("Сумма ренты " + a*40);}
+
+            =====================================================================================================*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+}
