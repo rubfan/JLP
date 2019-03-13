70e8b3174a2f45082f44a3d4fb3eb83255f90412
ilijah13 <igonin.ilya777@gmail.com>
2019-03-13 13:59:15 +0200

    Lesson 4 IGI

--- lesson_4/ilya.igonin/my_homework/HomeWorkL4.java
+++ lesson_4/ilya.igonin/my_homework/HomeWorkL4.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class HomeWorkL4 {
 
     Scanner in = new Scanner(System.in);
-
     int a = in.nextInt();
     int b = in.nextInt();
     int c = in.nextInt();
@@ -12,40 +11,26 @@ public class HomeWorkL4 {
 
     public static void main(String[] args) {
         HomeWorkL4 hw = new HomeWorkL4();
-
         hw.ex1();
-
         hw.ex2();
-
         hw.ex3();
-
         hw.ex4();
-
         hw.ex5();
-
         hw.ex6();
-
         hw.ex7();
     }
 
 
     private void ex1() {
-
         int min = Math.min(Math.min(a, b), c);
-
         System.out.println("Min num: " + min);
-
     }
 
 
     private void ex2() {
-
         int min = Math.min(Math.min(a, b), c);
-
         System.out.println("Min num: " + min);
-
         int max = Math.max(Math.max(a, b), c);
-
         System.out.println("Max num: " + max);
     }
 
@@ -53,22 +38,16 @@ public class HomeWorkL4 {
 
         if (a == b && b == c) {
             System.out.println("All numbers are equal");
-
         }
-
         else if (a > b && b == c) {
             System.out.println("Min numbers: " + b + ", " + c);
         }
-
         else if (b > a && a == c) {
             System.out.println("Min numbers: " + a + ", " + c);
         }
-
         else if (c > a && a == b) {
             System.out.println("Min numbers: " + a + ", " + b);
-
         }
-
     }
 
 
@@ -77,11 +56,9 @@ public class HomeWorkL4 {
         if ((a % 2) == 0) {
             System.out.println("Число " + a + " четное ");
         }
-
         else if ((a % 1) == 0) {
             System.out.println("Число " + a + " нечетное ");
         }
-
     }
 
 
@@ -90,7 +67,6 @@ public class HomeWorkL4 {
         if ((a % b) == 0) {
             System.out.println("Без остатка");
         }
-
         else if ((a % b) != 0) {
             System.out.println("С остатком");
         }
@@ -100,17 +76,12 @@ public class HomeWorkL4 {
 
         if (Math.abs(a - 10) < Math.abs(b - 10)) {
             System.out.println(a + " ближе к 10 чем " + b);
-
         }
-
         else if (Math.abs(a - 10) > Math.abs(b - 10)) {
             System.out.println(b + " ближе к 10 чем " + a);
-
         }
-
         else if (Math.abs(a - 10) == Math.abs(b - 10)) {
             System.out.println(a + " и " + b + " на одинаковом расстоянии от 10-ти");
-
 
         }
     }
@@ -122,29 +93,19 @@ public class HomeWorkL4 {
         int rentPrice;
 
         if (days <=5) {
-
             rentPrice = days * 40;
-
             System.out.println("Rent price: " + rentPrice);
         }
 
         else if(days > 5 && days <=7) {
-
             rentPrice = days * 40 - 20;
-
             System.out.println("Rent price: " + rentPrice);
         }
-
         else if(days > 7) {
-
             rentPrice = days * 40 - 50;
-
             System.out.println("Rent price: " + rentPrice);
-
         }
-
     }
-
 }
 
 
