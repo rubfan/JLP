269d58e44b4990c7fd18f0f27f69d0e7ccc7ab70
SojaVrobep <johndoe@example.com>
2019-04-06 21:09:32 +0300

    other directories have been deleted

--- lesson_10/nikita.palchinski/my_homework/doublemass/src/main/java/Lantern.java
+++
@@ -1,9 +0,0 @@
-public interface Lantern {
-    void setPower(Integer power);
-    void setState(State state);
-    void setNumofBat(Integer batteries);
-    String getColour();
-    Integer getPower();
-    State getState();
-    Integer getNumofBat();
-}
