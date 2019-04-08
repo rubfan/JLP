a8a1b610eb140ca5e70cfbdf69c8a75e9b033006
LeftN1 <47927040+LeftN1@users.noreply.github.com>
2019-04-08 21:11:09 +0300

    Update task1.java
    UP case

--- lesson_6/maxim.voroniuk/my_homework/task1.java
+++ lesson_6/maxim.voroniuk/my_homework/task1.java
@@ -1,4 +1,4 @@
-public class task1 {
+public class Task1 {
     public static void main(String[] args) {
 //        1) Создайте программу, выводящую на экран все четырёхзначные
 //        числа последовательности 1000 1003 1006 1009 1012 1015 ….
