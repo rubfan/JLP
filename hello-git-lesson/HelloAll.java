921c4413f3706607a541a8ea2da2c67b6f1309e0
Anna Tsurkan <annatsurkan.23@gmail.com>
2019-04-08 21:06:18 +0300

    update HelloAll

--- hello-git-lesson/HelloAll.java
+++ hello-git-lesson/HelloAll.java
@@ -5,9 +5,6 @@ public class HelloAll {
 	//================================
 	printUserInfo("Vladyslav", "Kasyanchuk");
 	printUserInfo("Ruslan", "Gramatic");    }
-
-	printUserInfo("Ruslan", "Gramatic");
-
 	printUserInfo("Irina", "Shvec");
 	printUserInfo("Rotar", "Denis");
 	printUserInfo("Maxim", "Voroniuk");
@@ -16,37 +13,12 @@ public class HelloAll {
   	printUserInfo("Vitalij", "Stinskij");
         printUserInfo("Iliya ","Chernomorets");
   	printUserInfo("Vladislav","Berdnikov");
-
-      printUserInfo("Anna","Tsurkan");
-        printUserInfo("Denis","Palamarchuk");
-
-      public static void main (String[] args){
-          System.out.println("Hello all!!!);
-          //================================
-          printUserInfo("Vladyslav", "Kasyanchuk");
-          printUserInfo("Ruslan", "Gramatic");
-          printUserInfo("Ilya", "Igonin");
-          printUserInfo("Irina", "Shvec");
-          printUserInfo("Rotar", "Denis");
-          printUserInfo("Maxim", "Voroniuk");
-          printUserInfo("Kirill" ,"Klyenko");
-          printUserInfo("Rotar", "Denis");
-          printUserInfo("Nikita", "Palchinski");
-          printUserInfo("Iliya ","Chernomorets");
-          printUserInfo("Vitalij", "Stinskij");
-          printUserInfo("Vladislav","Berdnikov");
-          printUserInfo("Anna","Tsurkan");
-          printUserInfo("Denis","Palamarchuk");
-          printUserInfo("Aleksey", "Andreev");
-      }
-
         printUserInfo("Anna","Tsurkan");
         printUserInfo("Denis","Palamarchuk");
 	printUserInfo("Aleksey", "Andreev");
 	printUserInfo("Ilya", "Igonin");
 	printUserInfo("Ivan", "Brus");
       }	
-
                            
       private static void printUserInfo(String firstName, String lastName) {
           System.out.format("Hello %s %s, Welcome to Hillel school !!!\n", firstName, lastName);
