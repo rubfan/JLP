e6db8037240f4354244dea0ea1e56129c3fc9170
LeftN1 <47927040+LeftN1@users.noreply.github.com>
2019-04-08 21:12:52 +0300

    Update task4.java
    UPcase

--- lesson_6/maxim.voroniuk/my_homework/task4.java
+++ lesson_6/maxim.voroniuk/my_homework/task4.java
@@ -1,4 +1,4 @@
-public class task4 {
+public class Task4 {
     public static void main(String[] args) {
 //        4) Создайте программу, выводящую на экран первые 20 элементов
 //        последовательности 2 4 8 16 32 64 128 ….
