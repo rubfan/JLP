38a3e5423423e107fca5560a57adaefe50d8b663
Irinashvec999 <irinashvec999@gmail.com>
2019-04-12 14:52:43 +0300

    try to clean my branch

--- lesson_5/vitalij.stinskij/my_homework/HomeWork5.java
+++ lesson_5/vitalij.stinskij/my_homework/HomeWork5.java
@@ -6,38 +6,40 @@ public class HomeWork5 {
         HomeWork5 m = new HomeWork5();
         m.calc();
         m.tip();
+
     }
 
-    private void calc() {
+    private void calc(){
         System.out.print("Select operation \n 1.addition \n 2.subtraction " +
                 "\n 3.multiplication \n 4.division \n ");
-
         int operation = in.nextInt();
         System.out.print("Input first num: ");
         int a = in.nextInt();
         System.out.print("Input second num: ");
-
         int b = in.nextInt();
-        switch (operation) {
+        switch (operation) 
+        {
             case 1:
                 System.out.println("Answer: " + (a + b));
                 break;
+
             case 2:
                 System.out.print("Answer: " + (a - b));
                 break;
             case 3:
                 System.out.println("Answer: " + (a * b));
                 break;
+
             case 4:
                 System.out.println("Answer: " + (a / b));
                 break;
              default:
                 System.out.println("Invalid choose of operation");
+
         }
     }
 
    private void tip() {
-     
        System.out.print("Input sum: ");
        int sum = in.nextInt();
         System.out.print("Select level of service: \n 1.terrible " +
@@ -48,15 +50,19 @@ public class HomeWork5 {
             case 1:
                 System.out.println("tip: " + 0);
                 break;
+
             case 2:
                 System.out.println("tip: " + (percent(sum,5)));
                 break;
+
             case 3:
                 System.out.println("tip: " + (percent(sum,10)));
                 break;
+
             case 4:
                 System.out.println("tip: " + (percent(sum,15)));
                 break;
+
             case 5:
                 System.out.println("tip: " + (percent(sum,20)));
                 break;
@@ -65,8 +71,10 @@ public class HomeWork5 {
         }
     }
 
-    private int percent(int sum, int per) {
+    private int percent(int sum, int per){
           sum = sum*per/100;
         return sum;
     }
+
 }
+
