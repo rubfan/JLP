1b76ec6417c2e9eb963b220b274e567f3c4ab39f
SojaVrobep <johndoe@example.com>
2019-04-14 14:49:41 +0300

    lesson 9 hw

---
+++ lesson_9/nikita.palchinski/my_homework/lesson_9.1/nikita.palchinski/my_homework/Normal Double Linked List/MyDoubleList.java
@@ -0,0 +1,14 @@
+public interface MyDoubleList {
+    void addl(Object data);
+    void addf(Object data);
+    void deletef();
+    void deletel();
+    void delete(int index);
+    void add(int index, Object data);
+    int getSize();
+    String showList();
+    Object getFirst();
+    Object getLast();
+    //Object[] transform();
+    String[] toArray();
+}
