fd983d4ba57d5708b416eac1da809ae5611d2ccf
Vlad Kasyanchuk <vladkasyanchuk@gmail.com>
2019-04-17 22:24:00 +0300

    lesson 8 has been done

---
+++ lesson_8/vlad.kasyanchuk/my_homework/selection sort/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] array = new int [100];
+        for (int i = 0; i < array.length; i++){
+            array[i] = (int)(Math.random()*100);
+            System.out.print(array[i] + " ");
+        }
+        long time = System.nanoTime();
+        for (int i = 0; i < array.length - 1; i++){
+            int min = array[i];
+            for (int j = i + 1; j < array.length; j++){
+                if (array[j] < array[i] ){
+                    int x = array[i];
+                    array[i] = array[j];
+                    array[j] = x;
+                }
+            }
+        }
+        time = System.nanoTime()-time;
+        System.out.println("");
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("");
+        System.out.println(time + " ns");
+    }
+}
---
+++ lesson_8/vlad.kasyanchuk/my_homework/selection sort/src/com/company/MergedArrays.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class MergedArrays {
+    public static void main (String[] args){
+        int [] a = new int[]{1, 3, 5};
+        int [] b = new int [] {2, 4, 8, 9, 12};
+        int[] c = new int[a.length + b.length];
+        int temp = 0;
+
+        for (int i = 0; i < a.length; i++){
+            c[i] = a[i];
+            temp++;
+        }
+        for (int i = 0; i < b.length; i++){
+            c[i+temp] = b[i];
+        }
+        for (int i = 0; i < c.length - 1; i++){
+            int min = c[i];
+            for (int j = i + 1; j < c.length; j++){
+                if (c[j] < c[i] ){
+                    int x = c[i];
+                    c[i] = c[j];
+                    c[j] = x;
+                }
+            }
+        }
+        for (int i = 0; i < c.length; i++){
+            System.out.print(c[i] + " ");
+        }
+    }
+}
