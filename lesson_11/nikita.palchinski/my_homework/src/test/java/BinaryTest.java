b9b17bf18c52c506a8ec5c90cd7d2c766b219045
SojaVrobep <johndoe@example.com>
2019-04-18 18:56:01 +0300

    lesson 11 hw

---
+++ lesson_11/nikita.palchinski/my_homework/src/test/java/BinaryTest.java
@@ -0,0 +1,34 @@
+import org.hamcrest.Matcher;
+import org.hamcrest.core.AllOf;
+import org.hamcrest.core.AnyOf;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsNot;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.hamcrest.CoreMatchers.*;
+import java.lang.String;
+import org.hamcrest.CoreMatchers;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.StringStartsWith.startsWith;
+import static org.hamcrest.CoreMatchers.is;
+
+public class BinaryTest {
+
+    @Before
+    public void init() {
+        Binary.bin(3);
+    }
+
+    @Test
+    public void isBinaryFormRight1() {
+        Assert.assertThat(Binary.binary, Is.is("11"));
+    }
+
+    @After
+    public void after() {
+    }
+}
