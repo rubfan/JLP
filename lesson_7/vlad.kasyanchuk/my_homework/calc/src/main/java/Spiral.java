2a5912e444a028df4d7cb594514c98d35989b902
Vlad Kasyanchuk <vladkasyanchuk@gmail.com>
2019-04-21 17:57:01 +0300

    split into methods

--- lesson_7/vlad.kasyanchuk/my_homework/calc/src/main/java/Spiral.java
+++ lesson_7/vlad.kasyanchuk/my_homework/calc/src/main/java/Spiral.java
@@ -3,64 +3,76 @@ public class Spiral {
         int a = 5;
         int b = 5;
         int next = 1;
-        int array1 [][] = new int [a][b];
+        int[][] array1 = new int[a][b];
+        fillPerimeter(array1, a, b, next);
+        fillTheMiddle(array1, a, b, next);
+        fillCenter(array1, a, b, next);
+        printArray(array1, a, b, next);
+    }
 
+    public static void fillPerimeter(int[][] array, int a, int b, int next) {
+        for (int x = 0; x < b; x++) {
+            array[0][x] = next;
+            next++;
+        }
+        for (int y = 1; y < a; y++) {
+            array[y][b - 1] = next;
+            next++;
+        }
+        for (int x = b - 2; x >= 0; x--) {
+            array[a - 1][x] = next;
+            next++;
+        }
+        for (int y = a - 2; y > 0; y--) {
+            array[y][0] = next;
+            next++;
+        }
+    }
 
-        for (int x = 0; x < b; x++){
-            array1[0][x] = next;
-            next ++;
-        }
-        for (int y = 1; y < a; y++){
-            array1[y][b - 1] = next;
-            next ++;
-        }
-        for (int x = b -2; x >= 0; x--){
-            array1[a - 1][x] = next;
-            next ++;
-        }
-        for (int y = a - 2; y > 0; y--){
-            array1 [y][0] = next;
-            next ++;
-        }
+    public static void fillTheMiddle(int[][] array, int a, int b, int next) {
         int c = 1;
         int d = 1;
-
-        while (next < a * b){
-            while (array1 [c][d + 1] == 0){
-                array1[c][d] = next;
+        while (next < a * b) {
+            while (array[c][d + 1] == 0) {
+                array[c][d] = next;
                 next++;
                 d++;
             }
-            while (array1[c + 1][d] == 0){
-                array1[c][d] = next;
+            while (array[c + 1][d] == 0) {
+                array[c][d] = next;
                 next++;
                 c++;
             }
-            while (array1[c][d - 1] == 0){
-                array1[c][d] = next;
+            while (array[c][d - 1] == 0) {
+                array[c][d] = next;
                 next++;
                 d--;
             }
-            while (array1 [c - 1][d] == 0){
-                array1[c][d] = next;
+            while (array[c - 1][d] == 0) {
+                array[c][d] = next;
                 next++;
                 c--;
             }
         }
-        for (int x = 0; x < a; x++){
-            for(int y = 0; y < b; y++){
-                if (array1[x][y] == 0){
-                    array1 [x][y] = next;
+    }
+
+    public static void fillCenter(int[][] array, int a, int b, int next) {
+        for (int x = 0; x < a; x++) {
+            for (int y = 0; y < b; y++) {
+                if (array[x][y] == 0) {
+                    array[x][y] = next;
                 }
             }
         }
-        for (int x = 0; x < b; x++) {
-            for (int y = 0; y < a; y++){
-                System.out.print(array1[x][y] + " ");
-            }
-            System.out.println("");
-        }
+    }
 
+    public static void printArray(int[][] array, int a, int b, int next) {
+        for (int x = 0; x < b; x++) {
+            for (int y = 0; y < a; y++) {
+                System.out.print(array[x][y] + " ");
+            }
+            System.out.println();
+        }
     }
-    }
+}
 
