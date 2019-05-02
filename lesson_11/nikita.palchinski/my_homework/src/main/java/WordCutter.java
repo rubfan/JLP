2e2f7e497ed016d227f6ff0318453b81ec700a06
Njuik It <42693451+NjuikIt@users.noreply.github.com>
2019-05-02 08:34:25 +0300

    Lesson 11 np (#97)
    * 9 lesson hw np
    
    * fdb
    
    * other directories have been deleted
    
    * tr
    
    * lesson 11 hw
    
    * lesson 11 hw
    
    * lesson 11 hw
    
    * clean
    
    * clear
    
    * resolved
    
    * .
    
    * Update HelloAll.java
    
    * Update Main.java
    
    * Update Gmail.java
    
    spaces

---
+++ lesson_11/nikita.palchinski/my_homework/src/main/java/WordCutter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class WordCutter {
+    public static void main(String args[])
+    {
+        String word;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your word: ");
+        word = sc.nextLine();
+        int size = word.length();
+        char firstLetter = word.charAt(0);
+        char lastLetter = word.charAt(size - 1);
+        String newWord = firstLetter + String.valueOf(size - 2) + lastLetter;
+        System.out.println("Your word in \"programmers' slang\": ");
+        System.out.println(newWord);
+        System.out.println(":)");
+    }
+}
