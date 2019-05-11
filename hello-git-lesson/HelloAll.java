b87f1991ca88e7f7201b0f77ef939cff2e18e49c
Vlad Kasyanchuk <vladkasyanchuk@gmail.com>
2019-05-11 20:05:39 +0300

    _

--- hello-git-lesson/HelloAll.java
+++ hello-git-lesson/HelloAll.java
@@ -1,28 +1,36 @@
 public class HelloAll {
 
-     public static void main (String[] args){
+     public static void main (String[] args) {
         System.out.println("Hello all!!!);
-
- Add fisrt and second name
-	//================================
-	printUserInfo("Vladyslav", "Kasyanchuk");
-	printUserInfo("Ruslan", "Gramatic");
-	printUserInfo("Irina", "Shvec");
-	printUserInfo("Rotar", "Denis");
+        //Add fisrt and second name
+	      //================================
+        printUserInfo("Vladyslav", "Kasyanchuk");
+        printUserInfo("Ruslan", "Gramatic");
+        printUserInfo("Irina", "Shvec");
+        printUserInfo("Rotar", "Denis");
         printUserInfo("Iliya ","Chernomorets");
-	printUserInfo("Maxim", "Voroniuk");
-	printUserInfo("Kirill" ,"Klyenko");
-	printUserInfo("Rotar", "Denis");
+        printUserInfo("Ruslan", "Gramatic");    }
+        printUserInfo("Irina", "Shvec");
+        printUserInfo("Rotar", "Denis");
+        printUserInfo("Maxim", "Voroniuk");
+        printUserInfo("Kirill" ,"Klyenko");
         printUserInfo("Nikita", "Palchinski");
-  	printUserInfo("Iliya ","Chernomorets");
-	printUserInfo("Vitalij", "Stinskij");
+        printUserInfo("Vitalij", "Stinskij");
         printUserInfo("Iliya ","Chernomorets");
-  	printUserInfo("Vladislav","Berdnikov");
+        printUserInfo("Vladislav","Berdnikov");
+        printUserInfo("Anna","Tsurkan");
         printUserInfo("Denis","Palamarchuk");
     }
-
-     }
+                           
     private static void printUserInfo(String firstName, String lastName) {
 	System.out.format("Hello %s %s, Welcome to Hillel school !!!\n", firstName, lastName);
     }
+        printUserInfo("Aleksey", "Andreev");
+        printUserInfo("Ilya", "Igonin");
+        printUserInfo("Ivan", "Brus");
+      }	
+                           
+      private static void printUserInfo(String firstName, String lastName) {
+          System.out.format("Hello %s %s, Welcome to Hillel school !!!\n", firstName, lastName);
+      }
 }
