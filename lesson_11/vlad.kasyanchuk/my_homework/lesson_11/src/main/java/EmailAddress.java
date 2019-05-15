83f7d4fd1f6e1ebb1a8bbc1094243838749663f3
Vlad Kasyanchuk <vladkasyanchuk@gmail.com>
2019-05-15 23:02:15 +0300

    task_2

--- lesson_11/vlad.kasyanchuk/my_homework/lesson_11/src/main/java/EmailAddress.java
+++ lesson_11/vlad.kasyanchuk/my_homework/lesson_11/src/main/java/EmailAddress.java
@@ -1,14 +1,21 @@
 public class EmailAddress {
-    String s;
 
-    EmailAddress(String s) {
-        this.s = s;
-    }
-
-    public String changeSymbols() {
+    public String changeSymbols(String s) {
         String s1 = s.replace("@", "[ at ]");
         String s2 = s1.replace(".", "[ dot ]");
         s = s2;
         return s;
     }
+
+    public String reduction(String a) {
+        if (a.length() <= 4) {
+            return a;
+        } else {
+            char[] array = a.toCharArray();
+            String b = "" + array[0] + (array.length - 2) + array[array.length - 1];
+            a = b;
+            return a;
+        }
+    }
+
 }
--- lesson_11/vlad.kasyanchuk/my_homework/lesson_11/src/test/java/TestEmailAddress.java
+++ lesson_11/vlad.kasyanchuk/my_homework/lesson_11/src/test/java/TestEmailAddress.java
@@ -3,11 +3,17 @@ import org.junit.Test;
 import org.hamcrest.core.Is;
 
 public class TestEmailAddress {
-    EmailAddress ea = new EmailAddress("vladkasyanchuk@gmail.com");
+    EmailAddress ea = new EmailAddress();
 
     @Test
-    public void testChangeSymbols (){
-        Assert.assertThat(ea.changeSymbols(), Is.is("vladkasyanchuk[ at ]gmail[ dot ]com"));
+    public void testChangeSymbols() {
+        Assert.assertThat(ea.changeSymbols("vladkasyanchuk@gmail.com"), Is.is("vladkasyanchuk[ at ]gmail[ dot ]com"));
+    }
+
+    @Test
+    public void testReduction() {
+        Assert.assertThat(ea.reduction("String"), Is.is("S4g"));
+        Assert.assertThat(ea.reduction("vlad"), Is.is("vlad"));
     }
 
 }
