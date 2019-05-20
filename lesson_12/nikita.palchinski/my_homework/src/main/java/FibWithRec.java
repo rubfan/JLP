9816a660cf024e553367050329a313f5bca0b9a1
Njuik It <42693451+NjuikIt@users.noreply.github.com>
2019-05-20 19:30:28 +0300

    Update FibWithRec.java

--- lesson_12/nikita.palchinski/my_homework/src/main/java/FibWithRec.java
+++ lesson_12/nikita.palchinski/my_homework/src/main/java/FibWithRec.java
@@ -1,7 +1,5 @@
 public class FibWithRec {
-
-    public static int fibr(int n)
-    {
+    public static int fibr(int n){
         if(n == 1)
             return 1;
         if(n == 2)
@@ -9,8 +7,7 @@ public class FibWithRec {
         else
             return fibr(n - 1) + fibr(n - 2);
     }
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         System.out.println(fibr(9));
     }
 }
