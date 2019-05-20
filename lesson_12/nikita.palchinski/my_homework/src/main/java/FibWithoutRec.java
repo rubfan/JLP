ba0573fe56cfd8059766289eb0fbf17a1bd4f326
Njuik It <42693451+NjuikIt@users.noreply.github.com>
2019-05-20 19:34:17 +0300

    Update FibWithoutRec.java

--- lesson_12/nikita.palchinski/my_homework/src/main/java/FibWithoutRec.java
+++ lesson_12/nikita.palchinski/my_homework/src/main/java/FibWithoutRec.java
@@ -1,7 +1,6 @@
 public class FibWithoutRec {
 
-    public static int fib(int n)
-    {
+    public static int fib(int n)[{
         int[] fibs = new int[n];
         fibs[0] = 1;
         fibs[1] = 1;
@@ -9,8 +8,7 @@ public class FibWithoutRec {
             fibs[i] = fibs[i - 1] + fibs[i - 2];
         return fibs[n - 1];
     }
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         System.out.println(fib(9));
     }
 }
