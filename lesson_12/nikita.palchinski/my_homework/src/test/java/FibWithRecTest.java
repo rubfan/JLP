062efdb4fec001ee898506236e9d019d96cf6dcf
Njuik It <42693451+NjuikIt@users.noreply.github.com>
2019-06-15 16:53:54 +0300

    Update FibWithRecTest.java

--- lesson_12/nikita.palchinski/my_homework/src/test/java/FibWithRecTest.java
+++ lesson_12/nikita.palchinski/my_homework/src/test/java/FibWithRecTest.java
@@ -1,33 +1,19 @@
-import org.hamcrest.Matcher;
-import org.hamcrest.core.AllOf;
-import org.hamcrest.core.AnyOf;
-import org.hamcrest.core.Is;
-import org.hamcrest.core.IsNot;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.hamcrest.CoreMatchers.*;
-import java.lang.String;
-import org.hamcrest.CoreMatchers;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNot.not;
-import static org.hamcrest.core.StringStartsWith.startsWith;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNot.not;
 
 public class FibWithRecTest {
 
     @Test
-    public void doesFibWorkRight()
-    {
-        Assert.assertThat(FibWithRec.fibr(9), Is.is(34));
-        Assert.assertThat(FibWithRec.fibr(9), IsNot.not(35));
+    public void doesFibWorkRight() {
+        assertThat(FibWithRec.fibr(9), is(34));
+        assertThat(FibWithRec.fibr(9), not(35));
     }
 
     @Test
-    public void fibComparator()
-    {
-        Assert.assertThat(FibWithRec.fibr(9), Is.is(FibWithoutRec.fib(9)));
+    public void fibComparator() {
+        assertThat(FibWithRec.fibr(9), is(FibWithoutRec.fib(9)));
     }
 }
