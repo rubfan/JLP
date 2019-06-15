a1762726809756d8672488013600a86a8e4c3ef1
Njuik It <42693451+NjuikIt@users.noreply.github.com>
2019-06-15 16:53:07 +0300

    Update FibWithoutRecTest.java

--- lesson_12/nikita.palchinski/my_homework/src/test/java/FibWithoutRecTest.java
+++ lesson_12/nikita.palchinski/my_homework/src/test/java/FibWithoutRecTest.java
@@ -1,27 +1,15 @@
-import org.hamcrest.Matcher;
-import org.hamcrest.core.AllOf;
-import org.hamcrest.core.AnyOf;
-import org.hamcrest.core.Is;
-import org.hamcrest.core.IsNot;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.hamcrest.CoreMatchers.*;
-import java.lang.String;
-import org.hamcrest.CoreMatchers;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNot.not;
-import static org.hamcrest.core.StringStartsWith.startsWith;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNot.not;
 
 public class FibWithoutRecTest {
 
     @Test
-    public void doesFibWorkRight()
-    {
-        Assert.assertThat(FibWithoutRec.fib(9), Is.is(34));
-        Assert.assertThat(FibWithoutRec.fib(9), IsNot.not(35));
+    public void doesFibWorkRight() {
+        assertThat(FibWithoutRec.fib(9), is(34));
+        assertThat(FibWithoutRec.fib(9), not(35));
+        assertThat(FibWithoutRec.fib(0), is(0));
     }
 }
