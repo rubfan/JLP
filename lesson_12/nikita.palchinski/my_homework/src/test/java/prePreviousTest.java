65976f3ce29cae88f9c12435a36187948afc2bff
Njuik It <42693451+NjuikIt@users.noreply.github.com>
2019-06-24 18:46:32 +0300

    Lesson 12 np (#101)
    * 9 lesson hw np
    
    * fdb
    
    * other directories have been deleted
    
    * tr
    
    * lesson 12 np hw
    
    * add
    
    * Update FibWithRec.java
    
    * Update FibWithoutRec.java
    
    * Update FibWithRec.java
    
    * Update FibWithoutRec.java
    
    * Update FibWithRec.java
    
    * Update prePrevious.java
    
    * Update FibWithoutRecTest.java
    
    * Update FibWithRecTest.java
    
    * Update prePreviousTest.java
    
    * Update pom.xml

---
+++ lesson_12/nikita.palchinski/my_homework/src/test/java/prePreviousTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNot.not;
+
+public class prePreviousTest {
+
+    int array[] = {2, 5, 1, 0, 99, 44, 12, 78};
+    int number1 = 12;
+    int number2 = 2;
+
+    @Before
+    public void before() {
+        prePrevious.quickSort(array, 0, array.length - 1);
+    }
+
+    @Test
+    public void doesPrePreviousRight() {
+        assertThat(prePrevious.prePreviousElement(array, number1), is(5));
+        assertThat(prePrevious.prePreviousElement(array, number2), not(5));
+    }
+}
